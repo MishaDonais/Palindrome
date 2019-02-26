b8d7cdb6c3868a4016d0abd23fbbc0e9ed14a24f
MishaDonais <43393229+MishaDonais@users.noreply.github.com>
2019-02-26 11:32:52 -0500

    Update Palindrome.java

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -15,21 +15,26 @@ public class Palindrome {
      */
     public static void main(String[] args) {
         
-        //Initializing JOptionPane
+        //Initializing JOptionPane 
         JOptionPane pane = new JOptionPane();
+        
+        //Taking input from the JOptionPane
         String input = JOptionPane.showInputDialog("Enter a word, and the program will determine if it is a palindrome");
         
-        //Empty string foe the backwards word
+        //Empty string to hold the backwards word
         String backwardsWord = "";
         
-        //Goes through the original string and reads it backwards into a new string
+        //Goes through the original string and reads it backwards into a new string one character at a time, making the second string the reverse of the first
         for (int pos = input.length()-1; pos >=0; pos--){
         backwardsWord += input.charAt(pos);
         }
-        //If it is a palindrome (string 1 and string 2 are the same)
+        //If it is a palindrome (string 1 and string 2 are the same), output that it is a palindrome
         if (input.equals(backwardsWord)){
             JOptionPane.showMessageDialog(null, "This is a palindrome", "Message", JOptionPane.INFORMATION_MESSAGE);
         }
+        //Otherwise, output that it is not a palindrome
+        else{
+            JOptionPane.showMessageDialog(null, "This is not palindrome", "Message", JOptionPane.INFORMATION_MESSAGE);
     
 }
 }
